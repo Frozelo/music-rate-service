d567fb36fb6002cb67c1960c63629267f66d6a9b
ivan <sizovivan.05@gmail.com>
2024-07-11 02:32:15 +0700

    zerolog add

--- main.go
+++ main.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	v1 "github.com/Frozelo/music-rate-service/internal/conrtoller/http/v1"
 	"github.com/Frozelo/music-rate-service/pkg/httpserver"
+	"github.com/Frozelo/music-rate-service/pkg/logger"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	l := logger.New("debug")
+
 	handler := gin.New()
 	v1.NewRouter(handler)
 
+	l.Info("starting new http server")
 	httpServer := httpserver.New(handler, httpserver.Port("8080"))
 
 	interrupt := make(chan os.Signal, 1)
@@ -23,14 +26,14 @@ func main() {
 
 	select {
 	case s := <-interrupt:
-		log.Print("app - Run - signal: " + s.String())
+		l.Info("app - Run - signal: " + s.String())
 	case err := <-httpServer.Notify():
-		log.Fatal((fmt.Errorf("app - Run - httpServer.Notify: %w", err)))
+		l.Error((fmt.Errorf("app - Run - httpServer.Notify: %w", err)))
 
 	}
 	err := httpServer.Shutdown()
 	if err != nil {
-		log.Fatal(fmt.Errorf("app - Run - httpServer.Shutdown: %w", err))
+		l.Error(fmt.Errorf("app - Run - httpServer.Shutdown: %w", err))
 	}
 
 }
