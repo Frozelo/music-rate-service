642a1c1edd2b39a79c0448b5427ae8c8145c0e88
ivansizov <sizovivan.05@gmail.com>
2024-07-15 01:25:40 +0700

    add basic transactions

--- internal/repository/postgres/music.go
+++ internal/repository/postgres/music.go
@@ -2,7 +2,6 @@ package postgres_repository
 
 import (
 	"context"
-	"errors"
 
 	"github.com/Frozelo/music-rate-service/internal/domain/entity"
 	"github.com/jackc/pgx/v4"
@@ -17,19 +16,30 @@ func NewMusicRepository(db *pgx.Conn) *musicRepository {
 }
 
 func (r *musicRepository) Create(ctx context.Context, music *entity.Music) (*entity.Music, error) {
-	query := `INSERT INTO musics (name, author) VALUES ($1, $2) RETURING name, author`
-	row := r.db.QueryRow(ctx, query, music.Name, music.Author)
-
-	var newMusic entity.Music
-	err := row.Scan(&newMusic.Name, newMusic.Author)
+	tx, err := r.db.Begin(ctx)
 	if err != nil {
 		return nil, err
 	}
-	return &newMusic, err
+	defer tx.Rollback(ctx)
+
+	query := `INSERT INTO musics (name, author) VALUES ($1, $2) RETURNING id, name, author, rate`
+	row := tx.QueryRow(ctx, query, music.Name, music.Author)
+
+	var newMusic entity.Music
+	err = row.Scan(&newMusic.Id, &newMusic.Name, &newMusic.Author, &newMusic.Rate)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := tx.Commit(ctx); err != nil {
+		return nil, err
+	}
+
+	return &newMusic, nil
 }
 
 func (r *musicRepository) FindById(ctx context.Context, id int) (*entity.Music, error) {
-	query := `SELECT id, name, author, rate FROM musics WHERE id = $1 `
+	query := `SELECT id, name, author, rate FROM musics WHERE id = $1`
 	row := r.db.QueryRow(ctx, query, id)
 
 	var music entity.Music
@@ -37,21 +47,30 @@ func (r *musicRepository) FindById(ctx context.Context, id int) (*entity.Music, 
 	if err := row.Scan(&music.Id, &music.Name, &music.Author, &music.Rate); err != nil {
 		return nil, err
 	}
+
 	return &music, nil
 }
 
 func (r *musicRepository) Update(ctx context.Context, music *entity.Music) error {
+	tx, err := r.db.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback(ctx)
+
 	query := `
         UPDATE musics
         SET name = $1, author = $2, rate = $3
         WHERE id = $4
     `
-	result, err := r.db.Exec(ctx, query, music.Name, music.Author, music.Rate, music.Id)
+	_, err = tx.Exec(ctx, query, music.Name, music.Author, music.Rate, music.Id)
 	if err != nil {
 		return err
 	}
-	if result.RowsAffected() == 0 {
-		return errors.New("music not found")
+
+	if err := tx.Commit(ctx); err != nil {
+		return err
 	}
+
 	return nil
 }
